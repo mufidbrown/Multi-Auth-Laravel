cbe96bfeec6b4aef2794b3f7eca2ac2222f3bc3d
mufidbrown <ilhammufid67@gmail.com>
2023-10-02 16:40:31 +0700

    add Seeder

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,35 +1,107 @@
 <?php
 
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Illuminate\Http\ReidrectResponse;
+use Illuminate\Http\RedirectResponse;
 
-class LoginController extends Controllers
+  
+
+class LoginController extends Controller
+
 {
-    // Login Controller
+
+    /*
+
+    |--------------------------------------------------------------------------
+
+    | Login Controller
+
+    |--------------------------------------------------------------------------
+
+    |
+
+    | This controller handles authenticating users for the application and
+
+    | redirecting them to your home screen. The controller uses a trait
+
+    | to conveniently provide its functionality to your applications.
+
+    |
+
+    */
+
+  
+
     use AuthenticatesUsers;
+
+  
+
+    /**
+
+     * Where to redirect users after login.
+
+     *
+
+     * @var string
+
+     */
 
     protected $redirectTo = RouteServiceProvider::HOME;
 
+  
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
     public function __construct()
+
     {
-        $this->middlew('guest')->except('logout');
+
+        $this->middleware('guest')->except('logout');
+
     }
 
+    
 
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return RedirectResponse
+
+     */
 
     public function login(Request $request): RedirectResponse
+
     {   
-        $input = $request->all(); 
+
+        $input = $request->all();
+
+     
 
         $this->validate($request, [
+
             'email' => 'required|email',
+
             'password' => 'required',
+
         ]);
+
+     
 
         if(auth()->attempt(array('email' => $input['email'], 'password' => $input['password'])))
 
@@ -57,5 +129,8 @@ class LoginController extends Controllers
 
         }
 
+          
+
     }
+
 }
--- database/seeders/CreateUsersSeeder.php
+++ database/seeders/CreateUsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class CreateUsersSeeder extends Seeder
 {
@@ -12,7 +13,10 @@ class CreateUsersSeeder extends Seeder
      */
     public function run(): void
     {
+       
+
         $users = [
+
             [
 
                'name'=>'Admin User',
@@ -24,32 +28,36 @@ class CreateUsersSeeder extends Seeder
                'password'=> bcrypt('123456'),
 
             ],
+
             [
 
-                'name'=>'Manager User',
- 
-                'email'=>'manager@itsolutionstuff.com',
- 
-                'type'=> 2,
- 
-                'password'=> bcrypt('123456'),
- 
-             ],
-             [
+               'name'=>'Manager User',
 
-                'name'=>'User',
- 
-                'email'=>'user@itsolutionstuff.com',
- 
-                'type'=>0,
- 
-                'password'=> bcrypt('123456'),
- 
-             ],
-            ];
-            foreach ($users as $key => $user) {
+               'email'=>'manager@itsolutionstuff.com',
 
-                User::create($user);
+               'type'=> 2,
+
+               'password'=> bcrypt('123456'),
+
+            ],
+
+            [
+
+               'name'=>'User',
+
+               'email'=>'user@itsolutionstuff.com',
+
+               'type'=>0,
+
+               'password'=> bcrypt('123456'),
+
+            ],
+
+        ];
+
+        foreach ($users as $key => $user) {
+
+            User::create($user);
         }
     }
 }
